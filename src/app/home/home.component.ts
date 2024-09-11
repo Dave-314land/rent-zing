195cfbc724bc44177c1c7cea8822a597008ea9b6
Dave-314land <dave.l.piland@gmail.com>
2024-09-11 16:56:52 -0400

    added housing location custom type

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-
+import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -9,5 +9,17 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = "https://angular.dev.assets/images/tutorials/common";
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test City',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  }
 
 }
