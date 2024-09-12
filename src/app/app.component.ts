a8659d60d5e02825ce2035804b659c48a0f73136
Dave-314land <dave.l.piland@gmail.com>
2024-09-12 17:32:57 -0400

    added home details component. Added functionality to grab id passed in url param

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HomeDetailsComponent } from './home-details/home-details.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, HomeComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HomeComponent, HomeDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './home/home.component';
+import { HomeComponent } from './home/home.component';
+import { HomeDetailsComponent } from './home-details/home-details.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
-        title: 'Home Page',
+        title: 'Home page',
+    },
+    {
+        path: 'details/:id',
+        component: HomeDetailsComponent,
+        title: 'Home details',
     },
 ];
