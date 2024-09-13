0df1e8f5aa16f9067a3cae43e8149561a93b3266
Dave-314land <dave.l.piland@gmail.com>
2024-09-13 15:56:52 -0400

    added form

--- src/app/home-details/home-details.component.ts
+++ src/app/home-details/home-details.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home-details',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './home-details.component.html',
   styleUrl: './home-details.component.css'
 })
@@ -15,8 +16,22 @@ export class HomeDetailsComponent {
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id'])
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId)
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    )
   }
 }
